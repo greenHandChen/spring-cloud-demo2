075612d2d986dce0f23c3229e70898197cfd52bc
enhui.chen <enhui.chen@china-hand.com>
2017-10-09 16:26:36 +0800

    a

---
+++ eureka-config-client/src/main/java/com/configClient/EurekaConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.configClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConfigClientApplication.class, args);
+	}
+}
